59f14e41506028adcb33c891833f6c00b669d50d
Ivan <116278074+SupStep@users.noreply.github.com>
2024-09-07 20:57:00 +0300

    Update productRoutes.js

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -50,36 +50,42 @@ router.delete('/:productId', productController.deleteOneProduct)
 
 router.post('/login', productController.authenticateUser)
 
-router.put('/:productId', upload.any(), async (req, res, next) => {
+router.put('/:productId', upload.fields([
+    { name: 'photos', maxCount: 10 },  // Для основного бокса
+    { name: 'items', maxCount: 50 }    // Для элементов бокса
+]), async (req, res, next) => {
     if (req.files && req.files.length > 0) {
         try {
-            // Обработка каждого файла
             await Promise.all(
-                req.files.map(async file => {
-                    const filename = Date.now() + path.extname(file.originalname);
-                    const uploadPath = path.join(__dirname, '../photos', filename);
+                Object.keys(req.files).map(async field => {
+                    const files = req.files[field];
+                    await Promise.all(
+                        files.map(async file => {
+                            const filename = Date.now() + path.extname(file.originalname);
+                            const uploadPath = path.join(__dirname, '../photos', filename);
 
-                    await sharp(file.buffer)
-			.rotate()
-                        .resize({ width: 600 })
-                        .toFormat('jpeg') // Можно изменить формат на другой
-                        .jpeg({ quality: 90 }) // Устанавливаем качество изображения
-                        .toFile(uploadPath);
+                            await sharp(file.buffer)
+                                .rotate()
+                                .resize({ width: 600 })
+                                .toFormat('jpeg')
+                                .jpeg({ quality: 90 })
+                                .toFile(uploadPath);
 
-                    // Обновляем имя файла в объекте
-                    file.filename = filename;
+                            // Обновляем имя файла
+                            file.filename = filename;
+                        })
+                    );
                 })
             );
-            // Переходим к следующему middleware
             next();
         } catch (error) {
             console.error('Error processing images:', error);
             return res.status(500).send('Error processing images');
         }
     } else {
-        // Если нет файлов, просто переходим к следующему middleware
         next();
     }
 }, productController.editProduct);
+
 
 module.exports = router
